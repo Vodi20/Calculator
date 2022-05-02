53b61b8932de799440f0f70f45ed51611eb86774
Vodi20 <marcel.vodenik@knapp.com>
2022-05-02 16:30:15 +0200

    Update Calculate.java

--- Calculator/src/calc/classes/Calculate.java
+++ Calculator/src/calc/classes/Calculate.java
@@ -1,8 +1,24 @@
 package calc.classes;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.function.BiFunction;
 
 public class Calculate {
+    public static final String PLUS = "+"; //const variablen immer in caps lock
+    public static final String MINUS = "-";
+    public static final String MULTIPLY = "*";
+    public static final String DIVIDE = "/";
+
+    public static final Map<String, BiFunction<Float, Float, Float>> OPERATIONS =
+            new HashMap<>() {{
+                put(Calculate.PLUS, (x,y) -> x+y);
+                put(Calculate.MINUS, (x,y) -> x-y);
+                put(Calculate.MULTIPLY, (x,y) -> x*y);
+                put(Calculate.DIVIDE, (x,y) -> x/y);
+            }};
+
 
     public static void Addition(){
         Scanner scanner = new Scanner(System.in);
